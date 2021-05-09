3a4491fe4080afa720a6c727d2cccc20a43ceff9
jdcneto <81934107+jdcneto@users.noreply.github.com>
2021-05-09 19:59:42 -0300

    Update model.py

--- model.py
+++ model.py
@@ -14,7 +14,9 @@ def build_model(im_shape, vocab_size, num_answers):
   x1 = Conv2D(32, 3, padding='same')(x1)
   x1 = BatchNormalization()(x1)
   x1 = MaxPooling2D()(x1)
- 
+  x1 = Flatten()(x1)
+  x1 = Dense(32, activation='relu')(x1)
+
   # The question network
   q_input = Input(shape=(vocab_size,))
   x2 = Dense(32, activation='relu')(q_input)
@@ -27,6 +29,6 @@ def build_model(im_shape, vocab_size, num_answers):
   out = Dense(num_answers, activation='softmax')(out)
 
   model = Model(inputs=[im_input, q_input], outputs=out)
-  model.compile(Adam(lr=5e-4), loss='categorical_crossentropy', metrics=['accuracy'])
+  model.compile(Adam(lr=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
 
   return model
