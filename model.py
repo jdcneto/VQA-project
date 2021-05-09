b96430a9eefd3e53f7e7a0f5ab7addb4fb375f64
jdcneto <81934107+jdcneto@users.noreply.github.com>
2021-05-09 17:49:53 -0300

    Update model.py

--- model.py
+++ model.py
@@ -1,25 +1,29 @@
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten, Multiply
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPooling2D, Flatten, Multiply, BatchNormalization
 from tensorflow.keras.optimizers import Adam
 
-def build_model(im_shape, vocab_size, num_answers, big_model):
+def build_model(im_shape, vocab_size, num_answers):
   # The CNN
   im_input = Input(shape=im_shape)
   x1 = Conv2D(8, 3, padding='same')(im_input)
+  x1 = BatchNormalization()(x1)
   x1 = MaxPooling2D()(x1)
   x1 = Conv2D(16, 3, padding='same')(x1)
+  x1 = BatchNormalization()(x1)
   x1 = MaxPooling2D()(x1)
-  x1 = Flatten()(x1)
-  x1 = Dense(32, activation='tanh')(x1)
-
+  x1 = Conv2D(32, 3, padding='same')(x1)
+  x1 = BatchNormalization()(x1)
+  x1 = MaxPooling2D()(x1)
+ 
   # The question network
   q_input = Input(shape=(vocab_size,))
-  x2 = Dense(32, activation='tanh')(q_input)
-  x2 = Dense(32, activation='tanh')(x2)
+  x2 = Dense(32, activation='relu')(q_input)
+  x2 = Dense(32, activation='relu')(x2)
 
   # Merge -> output
   out = Multiply()([x1, x2])
-  out = Dense(32, activation='tanh')(out)
+  out = Dense(32, activation='relu')(out)
+  # output
   out = Dense(num_answers, activation='softmax')(out)
 
   model = Model(inputs=[im_input, q_input], outputs=out)
