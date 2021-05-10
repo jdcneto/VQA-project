bb8557b1dd70ca29889ef4226553d3433217c1b1
jdcneto <81934107+jdcneto@users.noreply.github.com>
2021-05-10 02:37:21 -0300

    Update generate_data.py

--- gen_data/generate_data.py
+++ gen_data/generate_data.py
@@ -17,8 +17,8 @@ if not os.path.exists(path+'/data/test/images'):
 colors = list(Color)
 shapes = list(Shape)
 
-NUM_TRAIN = 4000
-NUM_TEST = 1000
+NUM_TRAIN = 8000
+NUM_TEST =  2000
 
 def create_data(image_path, num):
   qs = []
