1a3de551f670d376f715a59035c22670565dda52
jdcneto <81934107+jdcneto@users.noreply.github.com>
2021-05-10 03:56:18 -0300

    Update train.py

--- train.py
+++ train.py
@@ -5,11 +5,6 @@ from model import build_model
 from prepare_data import setup, plots
 
 
-if args.big_model:
-  print('Using big model')
-if args.use_data_dir:
-  print('Using data directory')
-
 # Prepare data
 train_X_ims, train_X_seqs, train_Y, test_X_ims, test_X_seqs, test_Y, im_shape, vocab_size, num_answers, _, _, _ = setup()
 
@@ -21,10 +16,11 @@ print('\n--- Training model...')
 history = model.fit(
   [train_X_ims, train_X_seqs],
   train_Y,
+  batch_size  = 64, 
   validation_data=([test_X_ims, test_X_seqs], test_Y),
   shuffle=True,
   epochs=100,
-  callbacks=[checkpoint],
+  callbacks=[checkpoint]  
 )   
 
 plots(history)
